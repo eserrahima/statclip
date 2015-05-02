74823270d7a37bc6e8ccc793f9ab1e1215c08ee9
eserrahima <edu_serrahima@hotmail.com>
2015-05-02 18:54:50 +0200

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,28 @@
+#Statclip
+#Eduard Serrahima, May 2015
+
+#ui.R
+#UI master file for Statclip
+
+library(shiny)
+library(shinydashboard)
+source("sidebar.R")
+source("body.R")
+
+dashboardPage(
+  
+  #HEADER: Title and dropdown menus
+  dashboardHeader(
+    title= "Statclip" #Title appearing on the upper left corner of the dashboard
+                      #Possibility of adding dropdownmenu outputs
+    ),
+  
+  #SIDEBAR: Menu with the different app functions.
+  #Imported from sidebar.R
+  sidebar,
+  
+  #BODY: Main content
+  #Imported from body.R
+  body
+
+  )
